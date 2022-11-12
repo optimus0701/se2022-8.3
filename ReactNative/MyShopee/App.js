fa75c71f334fb50e810f8f5f6d142d0fde8ce418
optimus0701 <vido0701@gmail.com>
2022-11-12 16:05:26 +0700

    optimus add login demo

--- ReactNative/MyShopee/App.js
+++ ReactNative/MyShopee/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <Text>Login Demo</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+      />
     </View>
   );
 }
@@ -16,5 +24,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
