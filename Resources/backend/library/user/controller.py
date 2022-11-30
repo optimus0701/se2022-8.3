4738d92f0d2a1b19145392b09987c0be011d6faa
hieuoneplus <nshd183@gmail.com>
2022-11-30 22:29:03 +0700

    add api for user

--- Resources/backend/library/user/controller.py
+++ Resources/backend/library/user/controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .services import add_user_service, get_user_by_username_service, get_all_user_service
+from .services import add_user_service, get_user_by_username_service, get_all_user_service, update_user_by_username_service, delete_user_by_username_service
 users = Blueprint("users", __name__)
 
 @users.route("/user-management/user", methods = ['POST'])
@@ -13,4 +13,12 @@ def get_user_by_username(username):
 @users.route("/user-management/users", methods = ['GET'])
 def get_all_user():
     return get_all_user_service()
+
+@users.route("/user-management/user/<String:username>", methods = ['PUT'])
+def update_user_by_username(username):
+    return update_user_by_username_service(username)
+
+@users.route("/user-management/user/<String:username>", methods = ['DELETE'])
+def delete_user_by_username(username):
+    return delete_user_by_username_service(username)
 
--- Resources/backend/library/user/services.py
+++ Resources/backend/library/user/services.py
@@ -39,3 +39,47 @@ def get_all_user_service():
         return users_schema.jsonify(users)
     else:
         return "Not found user"
+
+def update_user_by_username_service(username):
+    user = User.query.get(username)
+    data = request.json
+    if user:
+        if data and ('password' in data):
+            try:
+                user.password = data["password"]
+                db.session.commit()
+                return "password update"
+            except IndentationError:
+                db.session.rollback()
+                return "Cannot update user"
+        elif data and ('email' in data):
+            try:
+                user.email = data["email"]
+                db.session.commit()
+                return "email update"
+            except IndentationError:
+                db.session.rollback()
+                return "Cannot update user"
+        elif data and ('user_role' in data):
+            try:
+                user.user_role = data["user_role"]
+                db.session.commit()
+                return "user_role update"
+            except IndentationError:
+                db.session.rollback()
+                return "Cannot update user"
+    else:
+        return "Not found user"
+
+def delete_user_by_username_service(username):
+    user = User.query.get(username)
+    if user:
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            return "user deleted"
+        except IndentationError:
+            db.session.rollback()
+            return "Cannot delete user"
+    else:
+        return "Not found user"
