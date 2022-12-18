670cadb6dd8dcd369d87afce71aaba45e99ddb65
optimus0701 <vido0701@gmail.com>
2022-12-18 21:10:24 +0700

    fix login screen

--- ReactNative/MyShopee/LoginScreen.js
+++ ReactNative/MyShopee/LoginScreen.js
@@ -1,13 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export function LoginScreen({ navigation }) {
     return (
-      <View style={styles.title}>
-        <StatusBar style="auto"/>
-  
-        <Text>Login Demo</Text>
-  
+      <View style={styles.main}>
+        <Image style={styles.image} source={require('./assets/profile.png')}/>
+        
         <TextInput
           style={styles.input}
           placeholder="Username"/>
@@ -17,29 +14,36 @@ export function LoginScreen({ navigation }) {
           placeholder="Password"/>
   
         <Button 
-        style={styles.button}
-        title='Login'/>
+          style={styles.button}
+          title='Login'/>
+        
       </View>
     );
   }
 
 const styles = StyleSheet.create({
-    title: {
-      flex: 1,
+    main: {
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      flex: 1,
     },
     input: {
       height: 40,
       margin: 12,
+      width: 300,
       borderWidth: 1,
       padding: 10,
       borderRadius: 10,
       backgroundColor: '#76a2e8',
     },
     button: {
-      marginTop: 20
+      marginTop: 20,
     },
+    image: {
+      marginTop: 10,
+      marginBottom: 130,
+      width: 200,
+      height: 200,
+    }
   });
   
