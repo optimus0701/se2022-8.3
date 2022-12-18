e386fd5283c3b7b9c519dde867b81c8986866513
optimus0701 <vido0701@gmail.com>
2022-12-19 01:32:46 +0700

    fix forgetpassword screen

--- ReactNative/MyShopee/ForgetPasswordScreen.js
+++ ReactNative/MyShopee/ForgetPasswordScreen.js
@@ -1,43 +1,47 @@
 import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export function ForgetPasswordScreen({ navigation }) {
-    return (
-      <View style={styles.main}>
-        <Image style={styles.image} source={require('./assets/profile.png')}/>
+  return (
+    <View style={styles.main}>
+      <Image style={styles.image} source={require('./assets/profile.png')} />
 
-        <TextInput
-          style={styles.input}
-          placeholder="Email"/>
-  
-        <Button 
-          style={styles.button}
-          title='Send Request'/>
+      <TextInput
+        style={styles.input}
+        placeholder="Email" />
+
+      <Button
+        style={styles.button}
+        title='Send Request' />
+      <Text style={styles.text}>Login</Text>
       </View>
-    );
-  }
+  );
+}
 
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        flex: 1,
-      },
-      input: {
-        height: 40,
-        margin: 12,
-        width: 300,
-        borderWidth: 1,
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: '#76a2e8',
-      },
-      button: {
-        marginTop: 20,
-      },
-      image: {
-        marginTop: 10,
-        marginBottom: 120,
-        width: 200,
-        height: 200,
-      },
+  main: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    width: 300,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#76a2e8',
+  },
+  button: {
+    marginTop: 20,
+  },
+  image: {
+    marginTop: 10,
+    marginBottom: 120,
+    width: 200,
+    height: 200,
+  },
+  text: {
+    marginTop: 20,
+  }
 });
